65ec3f8a93603eeb6a3a106f16508ef82b72c1ff
markLosev <moshe.losev@yahoo.com>
2017-05-24 14:30:02 -0400

    Update ProperWords.java

--- Scraper/ProperWords.java
+++ Scraper/ProperWords.java
@@ -1,1 +1,25 @@
+package miniscraper;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Mark
+ */
+public class ProperWords extends JPanel {
+    JLabel properWord;
+    JTextArea properWordsText;
+    
+    public ProperWords() {
+        properWord = new JLabel("Proper words");
+        properWordsText = new JTextArea(15,15);
+        setLayout(new BorderLayout());
+        add(properWord, BorderLayout.NORTH);
+        add(properWordsText, BorderLayout.SOUTH);
+    }
+    
+}
 
